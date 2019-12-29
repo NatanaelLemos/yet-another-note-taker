4403da0a298e16f408c47a95b4b56ca39a53b199
Natanael Lemos <natanael.lemos@freshconsulting.com>
2019-12-29 11:12:11 -0800

    mac client

---
+++ src/client/NoteTaker.Client/NoteTaker.Client.Mac/AppDelegate.cs
@@ -0,0 +1,35 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace NoteTaker.Client.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        private NSWindow _window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled | NSWindowStyle.TexturedBackground | NSWindowStyle.UnifiedTitleAndToolbar;
+            var rect = new CoreGraphics.CGRect(200, 200, 800, 600);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            _window.Title = "NoteTaker";
+        }
+
+        public override NSWindow MainWindow => _window;
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
