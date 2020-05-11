4d681fb7c16217c31df79f6ed10249a908198e86
natanael lemos <natanaeltlemos@gmail.com>
2020-05-11 12:18:39 -0700

    mac client

---
+++ src/client/xamarin/YetAnotherNoteTaker.Mac/Main.cs
@@ -0,0 +1,14 @@
+﻿using AppKit;
+
+namespace YetAnotherNoteTaker.Mac
+{
+    internal static class MainClass
+    {
+        private static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
+            NSApplication.Main(args);
+        }
+    }
+}
