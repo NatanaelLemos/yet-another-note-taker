ce5f608a0fff2cbc35a1cab053ec9fec3adeaf53
natanael lemos <natanaeltlemos@gmail.com>
2020-06-03 10:02:58 -0700

    note creator and notebook editor

---
+++ src/client/blazor/YetAnotherNoteTaker.Blazor/wwwroot/js/quill_editor.js
@@ -0,0 +1,20 @@
+﻿window.startQuill = (body) => {
+    var quill = new Quill('#editor-container', {
+        modules: {
+            formula: true,
+            syntax: true,
+            toolbar: '#toolbar-container'
+        },
+        placeholder: '...',
+        theme: 'snow'
+    });
+
+    document.getElementById('btnTodo').onclick = () => {
+        quill.insertText(quill.getLength() - 1, '☐');
+        quill.setSelection(quill.getLength() - 1, 0);
+    }
+
+    document.querySelector('.ql-editor').innerHTML = body;
+}
+
+window.getQuillContent = () => document.querySelector('.ql-editor').innerHTML
