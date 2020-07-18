e4ec8f4a39526f17c20c41495aee395d090b3e53
natanael lemos <natanaeltlemos@gmail.com>
2020-07-18 09:53:11 -0700

    unit tests

---
+++ tests/client/System.Nxlx.Observer.UnitTests/ObserverBuilderTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace System.Nxlx.Observer.UnitTests
+{
+    public class ObserverBuilderTests
+    {
+        [Fact]
+        public void BuildShouldCreateAnInstanceOfIEventBroker()
+        {
+            var eventBroker = ObserverBuilder.Build();
+            eventBroker.Should().NotBeNull();
+        }
+    }
+}
---
+++ tests/client/System.Nxlx.Observer.UnitTests/ObserverExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace System.Nxlx.Observer.UnitTests
+{
+    public class ObserverExtensionsTests
+    {
+        [Fact]
+        public void AddObserverShouldAddAnInstanceOfIEventBroker()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddObserver();
+            serviceCollection.BuildServiceProvider()
+                .GetService<IEventBroker>().Should().NotBeNull();
+        }
+    }
+}
